948c13a987b3d6eb0b7c10229d771b6cf4aee8b6
Gabriel <gabrielherz.dev@gmail.com>
2026-05-03 13:16:32 +0200

    Instalador Repositorio packman corregido

--- core/zgh_install_packman.sh
+++ core/zgh_install_packman.sh
@@ -1,20 +1,20 @@
-
 #!/bin/bash
-# Script para configurar Packman y códecs en openSUSE Leap
+# Script para configurar Packman y códecs en openSUSE Leap 16.0
 
-echo "--- Configurando Repositorio Packman Essentials ---"
-# Añadir el repositorio con prioridad alta (90) para que zypper lo prefiera
-sudo zypper ar -cfp 90 "https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Leap_\$releasever/Essentials/" packman-essentials
+echo "--- Eliminando repositorio previo (si existe) ---"
+sudo zypper rr packman-essentials
 
-echo "--- Refrescando repositorios ---"
-sudo zypper ref
+echo "--- Configurando Repositorio Packman Essentials para Leap 16.0 ---"
+# Usamos la URL directa que está confirmada como operativa para Leap 16
+sudo zypper ar -cfgp 90 "http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Leap_16.0/Essentials/" packman-essentials
+
+echo "--- Refrescando repositorios e importando claves ---"
+sudo zypper --gpg-auto-import-keys ref
 
 echo "--- Realizando cambio de proveedor para códecs (Vendor Change) ---"
-# Este paso es CRUCIAL: cambia los paquetes de video de openSUSE a Packman
 sudo zypper dup --from packman-essentials --allow-vendor-change
 
-echo "--- Instalando códecs adicionales y FFmpeg ---"
-#sudo zypper install --allow-vendor-change ffmpeg-7 libavcodec-full vlc-#codecs gstreamer-plugins-libav gstreamer-plugins-bad gstreamer-plugins-ugly #gstreamer-plugins-good-extra
+echo "--- Instalando el patrón multimedia ---"
 sudo zypper install -t pattern multimedia
-echo "--- Proceso completado ---"
 
+echo "--- Proceso completado ---"
